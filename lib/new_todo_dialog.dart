4b564172ac38d2ce54eff54351202ced7b0db8aa
quyctd <quy.dc98@gmail.com>
2019-03-09 16:54:10 +0700

    Initial todolist app

---
+++ lib/new_todo_dialog.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/todo.dart';
+
+class NewTodoDialog extends StatelessWidget {
+  final titleController = new TextEditingController();
+  final detailController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    String title;
+    String detail = "";
+
+    var newTodoForm = Column(
+      children: <Widget>[
+        TextField(
+          controller: titleController,
+          decoration: InputDecoration(labelText: "Title"),
+          autofocus: true,
+        ),
+        TextField(
+          controller: detailController,
+          decoration: InputDecoration(labelText: "Detail"),
+          keyboardType: TextInputType.multiline,
+        )
+      ],
+    );
+
+    return AlertDialog(
+        title: Text("New todo"),
+        content: newTodoForm,
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Cancel"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          FlatButton(
+            child: Text("Add"),
+            onPressed: () {
+              final todo = new Todo(title: titleController.value.text, detail : detailController.value.text);
+              titleController.clear();
+              detailController.clear();
+              Navigator.of(context).pop(todo);
+            },
+          )
+        ]
+    );
+  }
+}
---
+++ lib/todo.dart
@@ -0,0 +1,7 @@
+class Todo {
+  Todo({this.title, this.isDone = false, this.detail = ""});
+
+  String title;
+  bool isDone;
+  String detail;
+}
